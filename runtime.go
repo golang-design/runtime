3188d5f36bece6d801d40e8522c7bea9541c9cb8
ouchangkun <hi@changkun.de>
2021-02-23 22:28:55 +0100

    feat: forward basic need to runtime

---
+++ runtime.go
@@ -0,0 +1,26 @@
+// Copyright 2021 The golang.design Initiative Authors.
+// All rights reserved. Use of this source code is governed
+// by a MIT license that can be found in the LICENSE file.
+//
+// Written by Changkun Ou <changkun.de>
+
+package runtime
+
+import "runtime"
+
+// Stack formats a stack trace of the calling goroutine into buf
+// and returns the number of bytes written to buf.
+// If all is true, Stack formats stack traces of all other goroutines
+// into buf after the trace for the current goroutine.
+func Stack(buf []byte, all bool) int {
+	return runtime.Stack(buf, all)
+}
+
+// NumCPU returns the number of logical CPUs usable by the current process.
+//
+// The set of available CPUs is checked by querying the operating system
+// at process startup. Changes to operating system CPU allocation after
+// process startup are not reflected.
+func NumCPU() int {
+	return runtime.NumCPU()
+}
